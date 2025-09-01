18efd26287bb759f5402cf9c1217c073c7b5ceaf
Pardner <sayings_shears3v@icloud.com>
2025-09-01 17:25:25 -0500

    Reorder modifier keys

--- keyboards/keebio/nyquist/rev3/keymaps/prdnr/keymap.c
+++ keyboards/keebio/nyquist/rev3/keymaps/prdnr/keymap.c
@@ -1,3 +1,4 @@
+#include "keycodes.h"
 #include QMK_KEYBOARD_H
 
 /* Todo: possible feature - replace '  ' with '.  ^shift' */
@@ -17,7 +18,7 @@ enum combos {
     /* Right Side. */
     ENTERBACKSPACE, SEMICOLONENTER, PBACKSPACE, SLASHSHIFT, OP,
     /* Space Area. */
-    LGUILCONTROL, SPACERCONTROL,
+    LOPTIONLCOMMAND, SPACERCONTROL,
     /* Quick Paren. */
     DF, FG, HJ, JK,
     /* Mouse. */
@@ -51,7 +52,7 @@ const uint16_t PROGMEM slashshift_combo[]     = {KC_SLSH, KC_RSFT, COMBO_END};
 const uint16_t PROGMEM op_combo[]             = {KC_O, KC_P, COMBO_END};
 
 /* Space Area. */
-const uint16_t PROGMEM lguilcontrol_combo[]  = {KC_LGUI, KC_LCTL, COMBO_END};
+const uint16_t PROGMEM loptionlcommand_combo[]  = {KC_LOPT, KC_LCMD, COMBO_END};
 const uint16_t PROGMEM spacercontrol_combo[] = {KC_SPC, KC_RCTL, COMBO_END};
 
 /* Quick Parens. */
@@ -95,7 +96,7 @@ combo_t key_combos[COMBO_COUNT] = {
     [SLASHSHIFT] = COMBO(slashshift_combo, KC_EQUAL),
     [OP] = COMBO(op_combo, KC_BACKSLASH),
     /* Space Area. */
-    [LGUILCONTROL] = COMBO(lguilcontrol_combo, KC_SPACE),
+    [LOPTIONLCOMMAND] = COMBO(loptionlcommand_combo, KC_SPACE),
     [SPACERCONTROL] = COMBO(spacercontrol_combo, KC_RALT),
     /* Quick Parens. */
     [DF] = COMBO(df_combo, KC_LEFT_BRACKET),
@@ -236,7 +237,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TD(TD_ESC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, /*split*/ KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
         TD(TD_NUMPAD_QMK), KC_A, KC_S, KC_D, KC_F, KC_G, /*split*/ KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, /*split*/ KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-        CW_TOGG, OSL(_FN), KC_GRV, KC_LALT, KC_LGUI, KC_LCTL, /*split*/ KC_SPC, KC_RCTL, KC_INS, DM_REC1, DM_RSTP, DM_PLY1),
+        CW_TOGG, OSL(_FN), KC_GRV, KC_LCTL, KC_LOPT, KC_LCMD, /*split*/ KC_SPC, KC_RCTL, KC_INS, DM_REC1, DM_RSTP, DM_PLY1),
 
     [_ARROWS] = LAYOUT_ortho_4x12(
         _______, _______, _______, _______, _______, _______, /*split*/ _______, _______, _______, _______, _______, _______,
